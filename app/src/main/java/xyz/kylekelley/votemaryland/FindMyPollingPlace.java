9ec31c83f9b6f29b5ebf90220e676c437e3190e5
Kyle Kelley <kkelley5@terpmail.umd.edu>
2016-04-14 18:22:15 -0400

    Added buttons on main activity for temporary navigation

---
+++ app/src/main/java/xyz/kylekelley/votemaryland/FindMyPollingPlace.java
@@ -0,0 +1,13 @@
+package xyz.kylekelley.votemaryland;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FindMyPollingPlace extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_my_polling_place);
+    }
+}
