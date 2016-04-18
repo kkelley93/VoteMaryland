28296dffb38348e72ce820d0aad598677bdc2b2d
tdpickeral <tj12123@yahoo.com>
2016-04-17 22:05:04 -0400

    working calander sqares

--- app/src/main/java/xyz/kylekelley/votemaryland/Calander_Of_Events.java
+++ app/src/main/java/xyz/kylekelley/votemaryland/Calander_Of_Events.java
@@ -1,7 +1,13 @@
 package xyz.kylekelley.votemaryland;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.roomorama.caldroid.CaldroidFragment;
+
+import java.util.Calendar;
+
 
 public class Calander_Of_Events extends AppCompatActivity {
 
@@ -12,5 +18,17 @@ public class Calander_Of_Events extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calander__of__events);
+        CaldroidFragment caldroidFragment = new CaldroidFragment();
+        Bundle args = new Bundle();
+        Calendar cal = Calendar.getInstance();
+        args.putInt(CaldroidFragment.MONTH, cal.get(Calendar.MONTH) + 1);
+        args.putInt(CaldroidFragment.YEAR, cal.get(Calendar.YEAR));
+        caldroidFragment.setArguments(args);
+
+        android.support.v4.app.FragmentTransaction t = getSupportFragmentManager().beginTransaction();
+        t.replace(R.id.calendarView, caldroidFragment);
+        t.commit();
+
+
     }
 }
