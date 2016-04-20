9e1e1f8e5896fa675b04c3f9bd96439a24ac15aa
Kyle Kelley <kkelley5@terpmail.umd.edu>
2016-04-20 01:05:03 -0400

    app wont start, appcompat theme issue

--- app/src/main/java/xyz/kylekelley/votemaryland/MainActivity.java
+++ app/src/main/java/xyz/kylekelley/votemaryland/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registration_webview);
+        setContentView(R.layout.activity_main);
     }
 
     public void launchCalendar(View v){
