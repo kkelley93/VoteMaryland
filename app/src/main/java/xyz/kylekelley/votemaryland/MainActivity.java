71badb049abfd6a03d1883ffb1711c7e1908af8a
Kyle Kelley <kkelley5@terpmail.umd.edu>
2016-04-21 21:50:39 -0400

    Final commit for presentation, just DB code & list view

--- app/src/main/java/xyz/kylekelley/votemaryland/MainActivity.java
+++ app/src/main/java/xyz/kylekelley/votemaryland/MainActivity.java
@@ -1,27 +1,31 @@
 package xyz.kylekelley.votemaryland;
 
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class MainActivity extends AppCompatActivity {
+    private ListView mDrawerList;
+    private ArrayAdapter<String> mAdapter;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+    private String mActivityTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void launchCandidateList(View v){
         Intent myIntent = new Intent(MainActivity.this, candidate_list.class);
         MainActivity.this.startActivity(myIntent);
+
     }
-
-
-
-
 
 }
