c3f615c043386c4e5bd8f0e414720fa459fa2148
Kyle Kelley <kkelley5@terpmail.umd.edu>
2016-05-16 17:12:28 -0400

    Added a ton of Google Civic API stuff

---
+++ app/src/main/java/xyz/kylekelley/votemaryland/asynctasks/CivicApiErrorResponse.java
@@ -0,0 +1,15 @@
+package xyz.kylekelley.votemaryland.asynctasks;
+
+import xyz.kylekelley.votemaryland.models.CivicApiError;
+
+/**
+ * Created by kathrynkillebrew on 7/17/14.
+ */
+public class CivicApiErrorResponse {
+    CivicApiError error;
+
+    public CivicApiError getError() {
+        return error;
+    }
+}
+
